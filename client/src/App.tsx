3bc5dfe8a6a87097ddc0894d99ab7dd0c1dd0fd7
esmin53 <esmin.tufekcic53@gmail.com>
2025-09-03 17:13:01 +0200

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen bg-red-200 box-content flex items-center justify-center'>
+      <div className='w-full h-24 bg-yellow-400'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
